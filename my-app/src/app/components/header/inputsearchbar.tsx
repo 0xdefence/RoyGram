b17704420fd8a8f630580c2e8c6a4598af00862b
wifimoney <elibelilty1998@gmail.com>
2026-07-13 15:59:24 +0100

    header convert to react

---
+++ my-app/src/app/components/header/inputsearchbar.tsx
@@ -0,0 +1,10 @@
+export function InputSearchBar() {
+    return (
+     <div className='input-search-bar'>
+        <div className="search-logo">
+            <i className="fa fa-search"></i>
+        </div>
+        <p>Search For</p>
+    </div>
+    )
+}
