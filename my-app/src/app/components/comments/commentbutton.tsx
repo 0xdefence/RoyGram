da07d5da460a88a85a11a0d5414dbc8c21d62ebe
wifimoney <elibelilty1998@gmail.com>
2026-07-19 11:44:56 +0100

    all HTML exported to InstaClone React

---
+++ my-app/src/app/components/comments/commentbutton.tsx
@@ -0,0 +1,8 @@
+export function InputSearchBar() {
+    return (
+    <div className="add-comment">
+    <i className="fa-solid fa-comment"></i>
+    <p>Add your own comment button</p>
+    </div>
+    )
+}
